500816e715fa8c249e4a346eba442e094c534552
selmakrgzz <selma.krgz@outlook.com>
2023-10-22 16:34:12 +0200

    Eksamensoppgaver excercises

---
+++ Algorithms/src/main/java/org/example/Eksamensoppgaver.java
@@ -0,0 +1,144 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Eksamensoppgaver {
+    public static String toString(int[] a){ //Eksamen 2014 konte
+        String output = "[";
+        if (a.length > 0){
+            for (int i = 0; i < a.length; i++){
+                output+=a[i];
+
+                if (i!=a.length-1){
+                    output+=", ";
+                }
+            }
+        }
+        output += "]";
+        return output;
+    }
+
+    public static void snu(int[] a){ //Eksamen 2016 konte
+        if (a.length > 0){
+          int i = 0;
+          int j = a.length-1;
+
+          while (i < j){
+              int temp = a[i];
+              a[i] = a[j];
+              a[j] = temp;
+              i++;
+              j--;
+          }
+        }
+    }
+
+    public static void frekvensFordeling(int[] a){ //Eksamen 2017
+        if (a[a.length-1] < a[0]){
+            throw new IllegalStateException("Arrayet er ikke sortert");
+        }
+        if (a.length == 0){
+            System.out.println(0+" "+0);
+        }
+
+        int temp = a[0];
+        int frekvens = 1;
+        int i = 1;
+
+        while (i < a.length){
+            if (temp == a[i]){
+                frekvens++;
+            } else if (temp != a[i]){
+                System.out.println(temp+" "+frekvens);
+                temp = a[i];
+                frekvens = 1;
+            }
+            i++;
+        }
+        System.out.println(temp+" "+frekvens);
+    }
+
+    public static int permutasjoner(int a){ //Eksamen 2020. I made it :DD
+        int sum = 1;
+        for (int i = 0; i < a; i++){
+            sum *= 6-i;
+        }
+        return sum;
+    }
+
+    static void storst(int[] a){ //Eksamen 2022
+        for (int i = 0; i < a.length-1; i++){
+            if (a[i] > a[i+1]){
+                int temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+            }
+        }
+    }
+
+    public static int[] snitt(int[] a, int[] b){
+        int length = Math.min(a.length, b.length);
+        int[] output = new int[length];
+
+        int antall = 0;
+
+        for (int i = 0; i < a.length; i++){
+            int verdi = a[i];
+            for (int j = 0; j < b.length; j++){
+                if (verdi == b[j]){
+                    output[antall++] = verdi;
+                }
+            }
+        }
+
+        return output;
+    }
+
+    public static String fjernDublikater(int[] a){ // Eksamen 2015 Made it :DD
+        int antall = 0;
+        int temp = a[0];
+        int[] output = new int[a.length];
+        output[0] = a[0];
+        int j = 1;
+        for (int i = 1; i < a.length; i++){
+            if (temp == a[i]){
+                antall++;
+                if (i != a.length-1){
+                    temp = a[i-1];
+                    if (temp == a[i]){
+                        antall++;
+                    }
+                }
+            } else {
+                temp = a[i];
+                output[j] = temp;
+                j++;
+            }
+        }
+        return "Utskrift: " +antall+ ": " +Arrays.toString(output);
+    }
+
+    public static void main(String []args){
+        int[] a = {};
+        int[] b = {5};
+        int[] c = {2,4,8,7,6,1};
+        int[] d = {1,3,5,5,6,8,8,8,9,10,10};
+        int[] e = {1,2,4,5,8,9};;
+        int[] f = {3,5,7,8,9,6};
+
+        System.out.println(toString(a)+ " " +toString(b)+ " " +toString(c));
+
+        snu(c);
+        System.out.println(Arrays.toString(c));
+
+        frekvensFordeling(d);
+        System.out.println(permutasjoner(6));
+
+        storst(c);
+        System.out.println(Arrays.toString(c));
+        System.out.println("Snitt: "+Arrays.toString(snitt(e, f)));
+
+        System.out.println(fjernDublikater(d));
+    }
+}
+
